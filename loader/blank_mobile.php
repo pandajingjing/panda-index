a3114166199cbaa7e95e3c82a0bc35a109f2efbb
xujing <xujing@jxulife.com>
2017-04-05 17:58:32 +0800

    完善web/bclient/service/system整套入口文件

--- loader/blank_mobile.php
+++ loader/blank_mobile.php
@@ -1,11 +1,15 @@
 <?php
 $G_PHP_DIR = array(
     PANDA_CODEPATH . '/blank-mobile',
+    PANDA_CODEPATH . '/blank-bclient',
+    PANDA_CODEPATH . '/blank-service',
     PANDA_CODEPATH . '/system'
 );
 
 $G_CONFIG_DIR = array(
     PANDA_CODEPATH . '/system/config',
+    PANDA_CODEPATH . '/blank-service/config',
+    PANDA_CODEPATH . '/blank-bclient/config',
     PANDA_CODEPATH . '/blank-mobile/config',
     PANDA_CODEPATH . '/config/blank-mobile'
 );
